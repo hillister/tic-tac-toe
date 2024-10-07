6030a3b79280cb00aebb41ceb8efa04e0f18180e
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-10-07 22:00:58 +0100

    Add function to check winning combinations

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const gameBoard = (function () {
-    let board = ["", "", "", "", "", "", "", "", ""];
+    let board = [
+        "", "", "", 
+        "", "", "", 
+        "", "", ""
+    ];
 
     function getBoard(){
         return board
@@ -24,3 +28,29 @@ const player = (function(name, marker) {
 const player1 = player("Player 1", "X");
 const player2 = player("Player 2", "O");
 
+//game logic
+
+const gamePlay = (function () {
+
+    function checkWin(board, marker){
+        winningCombo = [    
+            [0, 1, 2], 
+            [3, 4, 5], 
+            [6, 7, 8], 
+            [0, 3, 6], 
+            [1, 4, 7], 
+            [2, 5, 8], 
+            [0, 4, 8], 
+            [2, 4, 6]  
+        ]   
+    
+        for(combo in winningCombo){
+            if (gameBoard[combo[0]]){
+                return true
+            } else {
+                return false
+            }
+        }
+
+    }
+})
